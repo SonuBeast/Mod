549e067c730e9abb2a2c69e79889512d4c71bcf0
SonuBeast <67105554+SonuBeast@users.noreply.github.com>
2020-10-14 15:54:45 +0530

    Update m.js

--- m.js
+++ m.js
@@ -1,4 +1,7 @@
 let jdhksjh=document.createElement('a');
 jdhksjh.href='https://www.mrjaz.com/';
 document.body.appendChild(jdhksjh);
+if (Back_link.includes('SeoPro_Crack')) {
+            console.log('ok')
+        }
 console.clear();
