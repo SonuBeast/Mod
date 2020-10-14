bca9b01b4bb0b02f3a07255103e436034b04ff73
SonuBeast <67105554+SonuBeast@users.noreply.github.com>
2020-10-14 12:47:49 +0530

    Update m.js

--- m.js
+++ m.js
@@ -1,6 +1,4 @@
 let jdhksjh=document.createElement('a');
 jdhksjh.href='https://www.mrjaz.com/';
 document.body.appendChild(jdhksjh);
-
-document.querySelector('#header-ads-wrap').setAttribute('style','display:none;')
-//unos.innerHTML = 'Sonu';
+unos.innerHTML = 'Sonu';
