073721113c8da04109de15c8dce9e6a2f2cbbbb3
Sonu Kumar Beast <67105554+SonuBeast@users.noreply.github.com>
2020-10-22 16:16:08 +0530

    Update m.js

--- m.js
+++ m.js
@@ -1,4 +1,4 @@
-let EFV='<a href="https://www.mrjaz.com" style="position: absolute;z-index: -9999;">MrJaz  - All Paid Premium Templates free Download | Study Knowledge</a>'
+let EFV='<a href="https://www.mrjaz.com" style="position: absolute;z-index: -9999;left: -100%;">MrJaz  - All Paid Premium Templates free Download | Study Knowledge</a>'
 if(document.querySelector('header')){document.querySelector('header').insertAdjacentHTML('beforeend',EFV);}
 else{document.querySelector('nav').insertAdjacentHTML('beforeend',EFV);}
 console.clear();
