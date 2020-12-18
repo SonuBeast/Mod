cd31b01ba06fdfeca5d5579f72190d5e70aa0483
Sonu Kumar Beast <67105554+SonuBeast@users.noreply.github.com>
2020-12-18 18:03:07 +0530

    Update m.js

--- m.js
+++ m.js
@@ -2,6 +2,5 @@ let EFV='<a href="https://www.am2z.com" style="position: absolute;z-index: -9999
 if(document.querySelector('header')){document.querySelector('header').insertAdjacentHTML('beforeend',EFV);}
 else{document.querySelector('body').insertAdjacentHTML('beforeend',EFV);}
 console.clear();
-if(location.href=='https://www.sitedroos.com/2015/09/500_12.html' || location.href=='https://www.9jaedublog.com.ng/2020/06/betking-virtual-football-cheat-2020.html'){window.open("https://www.mrjaz.com/2020/11/business-startup-kaise-karana-chaahie.html");}
-if(location.href=='https://www.theprabhakar.in/2016/05/how-to-use-nitrotyper-21-nitro-type-speed-hack.html' || location.href=='https://www.sitedroos.com/2017/01/More-Words-Commonly-used-francais-300-1000-500-Words.html'){window.open("https://www.mrjaz.com/2020/11/artificial-intelligence-kaise-kam-karta-hai.html");}
-if(location.href=='https://www.bollywooddjsclub.co.in'){window.open("https://www.mrjaz.com/2020/11/bsc-zoology-kya-hai.html");}
+
+/*if(location.href=='' || ){window.open('')}*/
