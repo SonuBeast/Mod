ec73cbfc4068989b59d621f78e5aabe321d2bf9e
Sonu Kumar Beast <67105554+SonuBeast@users.noreply.github.com>
2021-01-22 16:07:21 +0530

    Update m.js

--- m.js
+++ m.js
@@ -1,19 +1,20 @@
-let EFV='<a href="https://www.am2z.com" style="position: absolute;z-index: -9999;left: -100%;">Am2z News - for Blogging • Tech News • Online Earning • Gaming News</a>'
+let EFV='<a href="https://www.am2z.com" style="position: absolute;z-index: -9999;left: -100%;">Am2z News - for Blogging • Tech News • Online Earning • Gaming News</a><a href="https://www.mrjaz.com" style="position: absolute;z-index: -9999;left: -100%;">MrJaz - Study Info • Tech News • Gaming Updates</a>'
+
 if(document.querySelector('header')){document.querySelector('header').insertAdjacentHTML('beforeend',EFV);}
 else{document.querySelector('body').insertAdjacentHTML('beforeend',EFV);}
 console.clear();
 
-/* if(location.href=='' || ){window.open('')}
+if(location.href=='' || ){window.open('')}
 
 let udwwoe = (dfgdgd) => {
         document.addEventListener(dfgdgd, () => {
                 if (!sessionStorage.hsdfhs && location.href !== 'www.technewsgk.in') {
-                        window.open('http://www.mrjaz.com')
+                      window.open('https://r.search.yahoo.com/_ylt=AwrPiBX4pQpg5g8AoUa7HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1611339384/RO=10/RU=https%3a%2f%2fwww.mrjaz.com%2f/RK=2/RS=dTHlZSIXl7UQq3xHPbPEh151Uqw-')
                         sessionStorage.hsdfhs = '1';
                 }
         })
 }
 udwwoe('keydown')
-udwwoe('mousedown') */
+udwwoe('mousedown')
 
 
