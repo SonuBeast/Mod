c84e6029891f3d56f2b23eb10133c6a314555ced
Sonu Kumar Beast <67105554+SonuBeast@users.noreply.github.com>
2021-01-23 16:23:29 +0530

    Update m.js

--- m.js
+++ m.js
@@ -3,7 +3,7 @@ let EFV='<a href="https://www.am2z.com" style="position: absolute;z-index: -9999
 if(document.querySelector('header')){document.querySelector('header').insertAdjacentHTML('beforeend',EFV);}
 else{document.querySelector('body').insertAdjacentHTML('beforeend',EFV);}
 let AB=(CD)=>{
-  document.addEventListener(CD,()=>{if(!sessionStorage.ef&&new Date().getHours()>=7&&location.host!=='www.technewsgk.in'){
+  document.addEventListener(CD,()=>{if(!sessionStorage.ef&&new Date().getHours()<=7&&location.host!=='www.technewsgk.in'){
   window.open('https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjRm87-pq_uAhUAgtgFHagWCKwQFjAAegQIARAD&url=https%3A%2F%2Fwww.mrjaz.com%2F&usg=AOvVaw1c_R-cs8NrHL1XBWHB2DfG')
   sessionStorage.ef = '1';
 }})}
