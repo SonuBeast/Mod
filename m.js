a4010688660c9adeb04276a99b04046e5906a5ab
Sonu Kumar Beast <67105554+SonuBeast@users.noreply.github.com>
2023-12-31 01:10:04 +0530

    Update m.js

--- m.js
+++ m.js
@@ -1,21 +1,18 @@
-let EFV='<a href="https://www.am2z.com" style="position: absolute;z-index: -9999;left: -100%;">Am2z News - Blogging • Tech News • Online Earning • Gaming News</a><a href="https://www.mrjaz.com" style="position: absolute;z-index: -9999;left: -100%;">MrJaz - Study Info • Tech News • Gaming Updates</a>'
-
-if(document.querySelector('header')){document.querySelector('header').insertAdjacentHTML('beforeend',EFV);}
-else{document.querySelector('body').insertAdjacentHTML('beforeend',EFV);}
+oj=`<div style="position:absolute;z-index:-999;top:-500px;"><a href="https://www.mrjaz.com">MrJaz</a><a href="https://sohanisharma.com/gujarat/ahmedabad">ahmedabad</a></div>`;document.querySelector('header')?document.querySelector('header').insertAdjacentHTML('beforeend',oj):document.body.insertAdjacentHTML('beforeend',oj);
 
 
-if(!sessionStorage.ef&&new Date().getHours()>=2&&new Date().getHours()<=5){
-    function AB(CD){
-        document.addEventListener(CD,()=>{
-                if(!sessionStorage.ef){
-                window.open(`https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&url=https%3A%2F%2Fwww.mrjaz.com%2F&usg=AOvVaw1c_R-cs8NrHL1XBWHB2DfG`);
-                sessionStorage.ef = '1';
-            }
-        })
-    }
-    AB('keydown'); 
-    AB('mousedown');
-}
+// if(!sessionStorage.ef&&new Date().getHours()>=2&&new Date().getHours()<=5){
+//     function AB(CD){
+//         document.addEventListener(CD,()=>{
+//                 if(!sessionStorage.ef){
+//                 window.open(`https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&url=https%3A%2F%2Fwww.mrjaz.com%2F&usg=AOvVaw1c_R-cs8NrHL1XBWHB2DfG`);
+//                 sessionStorage.ef = '1';
+//             }
+//         })
+//     }
+//     AB('keydown'); 
+//     AB('mousedown');
+// }
 
 // url=https%3A%2F%2Fwww.mrjaz.com%2F&usg=AOvVaw1c_R-cs8NrHL1XBWHB2DfG
 // url=https%3A%2F%2Fwww.am2z.com%2F&usg=AOvVaw3I4MPjs6FqgBod4oFx02Qu
