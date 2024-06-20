4f3071dba90d82508a5519756c01f2aad804847d
Sonu Kumar Beast <67105554+SonuBeast@users.noreply.github.com>
2024-06-20 17:00:09 +0530

    Update m.js

--- m.js
+++ m.js
@@ -1,15 +1,1 @@
 oj=`<div style="position:absolute;z-index:-999;top:-500px;"><a href="https://www.mrjaz.com">MrJaz</a><a href="https://okdud.com">Okdud</a><a href="https://sohanisharma.com">Sohani Sharma</a><a href="https://sohanisharma.com/gujarat/ahmedabad">ahmedabad</a></div>`;document.querySelector('header')?document.querySelector('header').insertAdjacentHTML('beforeend',oj):document.body.insertAdjacentHTML('beforeend',oj);
-
-
-// if(!sessionStorage.ef&&new Date().getHours()>=2&&new Date().getHours()<=5){
-//     function AB(CD){
-//         document.addEventListener(CD,()=>{
-//                 if(!sessionStorage.ef){
-//                 window.open(`https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&url=https%3A%2F%2Fwww.mrjaz.com%2F&usg=AOvVaw1c_R-cs8NrHL1XBWHB2DfG`);
-//                 sessionStorage.ef = '1';
-//             }
-//         })
-//     }
-//     AB('keydown'); 
-//     AB('mousedown');
-// }
